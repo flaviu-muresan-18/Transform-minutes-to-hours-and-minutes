0a397100f49d55f8b946aaba5fe0a1efaa93ba86
Flaviu Muresan <80217168+flaviu-muresan-18@users.noreply.github.com>
2023-02-26 13:28:05 +0200

    Add files via upload
    Correct file

--- MinutesConvertor.cs
+++ MinutesConvertor.cs
@@ -7,6 +7,12 @@ public class MinutesConvertor
         System.Console.WriteLine("Please enter how minutes have you worked.");
         int minutes = int.Parse(Console.ReadLine());
 
-        
+        //Conversion to hours and minutes
+
+        int hours = minutes / 60;
+
+        int newMinutes = minutes % 60;
+
+        Console.WriteLine("You worked {0} hours and {1} minutes.", hours, newMinutes);
     }
 }
