348cc82f93d6225df174eb1cf92d827b06592402
Flaviu Muresan <80217168+flaviu-muresan-18@users.noreply.github.com>
2023-02-26 13:20:58 +0200

    First verion of the project

---
+++ MinutesConvertor.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class MinutesConvertor
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Please enter how minutes have you worked.");
+        int minutes = int.Parse(Console.ReadLine());
+
+        
+    }
+}
